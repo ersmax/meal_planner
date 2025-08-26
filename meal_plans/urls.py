00df440f89b30a41e8c29a3ab0b6347aa91e650c
ersmax <e.masarin@student.vu.nl>
2025-08-26 23:11:13 +0200

    Created webpages meals and index

--- meal_plans/urls.py
+++ meal_plans/urls.py
@@ -1,13 +1,15 @@
-"""Define URL patterns for meal_plans."""
+"""Defines URL patterns for meal_plans."""
+
 from django.urls import path
+
 from . import views
 
-app_name = "meal_plans"
+app_name = 'meal_plans'
 urlpatterns = [
-    # Home page
+    # Home page.
     path('', views.index, name='index'),
-    # Page to view all the meals
+    # Page to view all meals.
     path('meals/', views.meals, name='meals'),
-    # Page to view individual food
-    path('meal/<int: meal_id>/', views.meal, name='meal'),
+    # Page to view individual meals.
+    path('meal/<int:meal_id>/', views.meal, name='meal'),
 ]
